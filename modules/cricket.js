746a15cffb7e1af598c5b6d1ce4ccc17ef7b0635
lelumthikshana <85168770+lelumthikshana@users.noreply.github.com>
2021-06-06 20:05:25 +0530

    Update cricket.js

--- modules/cricket.js
+++ modules/cricket.js
@@ -6,7 +6,7 @@ async function cricket(url) {
         if (scoredata.success) {
             return {
                 status: true,
-                msg: `🏏 *${scoredata.livescore.title}*\n\nTotal: ${scoredata.livescore.current}\n\n\nBATMANS DETAILS\n\nBatman: ${scoredata.livescore.batsman} - ${scoredata.livescore.batsmanrun} (${scoredata.livescore.ballfaced})\nFours: ${scoredata.livescore.fours}\nSixes: {scoredata.livescore.sixes}\n\n*Commentary 👇*\n\n${scoredata.livescore.commentary.map(cmntry => {
+                msg: `🏏 *${scoredata.livescore.title}*\n\nTotal: ${scoredata.livescore.current}\n\n*BATMANS DETAILS*\nBatman: ${scoredata.livescore.batsman} - ${scoredata.livescore.batsmanrun} (${scoredata.livescore.ballfaced})\nFours: ${scoredata.livescore.fours}\nSixes: ${scoredata.livescore.sixes}\n\nPartnership: ${scoredata.livescore.partnership}\nRecent balls: ${scoredata.livescore.recentballs}\n\nLast Wicket: ${scoredata.livescore.lastwicket}\n\nRun rate: ${scoredata.livescore.runrate}\n\n\n\n*BOWLER DETAILS*\n\nBowler: ${scoredata.livescore.bowler}\nOvers: ${scoredata.livescore.bowlerover}\nRuns: ${scoredata.livescore.bowlerruns}\nWickets: ${scoredata.livescore.bowlerwickets}\n\n*Commentary 👇*\n\n${scoredata.livescore.commentary.map(cmntry => {
                     return cmntry
                 })}`,
             }
