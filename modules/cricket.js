dba44e66e55a1a5b7fa2a6e5834c19dae1821cba
lelumthikshana <85168770+lelumthikshana@users.noreply.github.com>
2021-06-07 16:10:16 +0530

    Update cricket.js

--- modules/cricket.js
+++ modules/cricket.js
@@ -6,7 +6,7 @@ async function cricket(url) {
         if (scoredata.success) {
             return {
                 status: true,
-                msg: `🏏 *${scoredata.livescore.title}*\n\n_UPDATE_ «${scoredata.livescore.update}»\n\n_*Total*_: ${scoredata.livescore.current}\n\n\n\n*BATMANS DETAILS*\n\nBatman: ${scoredata.livescore.batsman} - ${scoredata.livescore.batsmanrun}\nBall faced: ${scoredata.livescore.ballfaced})\nFours: ${scoredata.livescore.fours}\nSixes: ${scoredata.livescore.sixes}\n\nPartnership: ${scoredata.livescore.partnership}\nRecent balls: ${scoredata.livescore.recentballs}\n\nLast Wicket: ${scoredata.livescore.lastwicket}\n\nRun rate: ${scoredata.livescore.runrate}\n\n\n\n*BOWLER DETAILS*\n\nBowler: ${scoredata.livescore.bowler}\nOvers: ${scoredata.livescore.bowlerover}\nRuns: ${scoredata.livescore.bowlerruns}\nWickets: ${scoredata.livescore.bowlerwickets}
+                msg: `~RangeR~\n🏏 *${scoredata.livescore.title}*\n\n_UPDATE_ «${scoredata.livescore.update}»\n\n_*Total*_: ${scoredata.livescore.current}\n\n\n\n*BATMANS DETAILS*\n\nBatman: ${scoredata.livescore.batsman} - ${scoredata.livescore.batsmanrun}\nBall faced: ${scoredata.livescore.ballfaced})\nFours: ${scoredata.livescore.fours}\nSixes: ${scoredata.livescore.sixes}\n\nPartnership: ${scoredata.livescore.partnership}\nRecent balls: ${scoredata.livescore.recentballs}\n\nLast Wicket: ${scoredata.livescore.lastwicket}\n\nRun rate: ${scoredata.livescore.runrate}\n\n\n\n*BOWLER DETAILS*\n\nBowler: ${scoredata.livescore.bowler}\nOvers: ${scoredata.livescore.bowlerover}\nRuns: ${scoredata.livescore.bowlerruns}\nWickets: ${scoredata.livescore.bowlerwickets}
                     
                       by 🏏 𝐂𝐫𝐢𝐜 𝐓𝐚𝐥𝐤𝐬 🎤
                 `,
